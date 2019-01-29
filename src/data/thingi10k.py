6766af50050ff7a18cabd3c2d613e0a08efae7ac
jcworkma <jack.workman@intel.com>
2019-01-29 00:06:36 -0800

    make thingi10k index

---
+++ src/data/thingi10k.py
@@ -0,0 +1,118 @@
+import urllib.request
+import requests
+import shutil
+import json
+import csv
+import os
+
+
+def api_json(url):
+    """
+    Returns json data from the provided url
+
+    Args:
+        url: str
+
+    Returns:
+        json as dict or None
+    """
+    print('api_query: {}'.format(url))
+    resp = requests.get(url=url)
+    data = None
+    if resp.status_code != 200:
+        print('failed to retrieve data from {0}'.format(url))
+        print('status_code={0}'.format(resp.status_code))
+    else:
+        data = resp.json()
+    return data
+
+
+def thingi10k_api_data(obj_id):
+    """
+    Returns api data of specified thingi10k object
+
+    Args:
+        obj_id: int or str
+    """
+    url = 'https://ten-thousand-models.appspot.com/api/v1/model/{}'.format(obj_id)
+    return api_json(url)
+
+
+def thingi10k_image(obj_id, dest):
+    """
+    Returns an image file for the specified thingi10k object
+
+    https://stackoverflow.com/questions/3042757/downloading-a-picture-via-urllib-and-python
+    https://docs.python.org/3.0/library/urllib.request.html#urllib.request.urlretrieve
+
+    Args:
+        obj_id: int or str
+        dest: str, path to save image file to
+
+    Returns:
+        str, path at which image file was saved
+    """
+    url = 'https://storage.googleapis.com/thingi10k/renderings/{}.png'.format(obj_id)
+    print('img_query: {}'.format(url))
+    filename, headers = urllib.request.urlretrieve(url)
+    shutil.move(filename, dest)
+    return dest
+
+
+def make_thingi10k_index(index_path):
+    """
+    Constructs a csv index of thingi10k objects
+    """
+    rows = list()
+    header = ['file', 'name', 'num_vertices']
+
+    mesh_dir = '../data/external/Thingi10k/raw_meshes'
+    raw_dir = '../data/raw'
+    proc_dir = '../data/processed'
+
+    fields = list()
+    data = list()
+
+    for path in os.listdir(mesh_dir)[:10]:
+
+        # quick check that we are reading something that we care about
+        if not path.endswith('.stl'):
+            print('not an stl file: {}'.format(path))
+            continue
+
+        # get file ids
+        obj_id = os.path.splitext(path)[0]
+
+        # get api data
+        file_data = thingi10k_api_data(obj_id)
+        stl_name = '{}.json'.format(obj_id)
+        with open(os.path.join(raw_dir, stl_name), 'w') as outfile:
+            json.dump(data, outfile)
+
+        # gather object images
+        img_name = '{}.png'.format(obj_id)
+        dest = os.path.join(raw_dir, img_name)
+        dest = thingi10k_image(obj_id, dest)
+
+        # done
+        file_data['stl_file'] = stl_name
+        file_data['img_file'] = img_name
+        fields = list(set(fields).union(set(file_data.keys())))
+        data.append(file_data)
+
+    # write index
+    # index_path = '_output/thingi10k.csv'
+    with open(index_path, 'w', newline='') as csvfile:
+
+        writer = csv.DictWriter(csvfile, fieldnames=fields)
+
+        writer.writeheader()
+        for i, obj in enumerate(data):
+            # https://stackoverflow.com/questions/1285911/how-do-i-check-that-multiple-keys-are-in-a-dict-in-a-single-passs
+            # make sure that this obj has all the right fields
+            if set(fields) <= set(obj):
+                for f in fields:
+                    if not obj.get(f, None):
+                        obj[f] = None
+
+            writer.writerow(obj)
