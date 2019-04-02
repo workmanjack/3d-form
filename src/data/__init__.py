567dff7214ee9460c2f625b120723a8e5c584221
jcworkma <jack.workman@intel.com>
2019-04-02 16:31:07 -0700

    quick notebook to demo recons and combos

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -22,3 +22,4 @@ MODELNET10_TOILET_NOROTATION_INDEX = os.path.join(PROCESSED_DIR, 'modelnet10_toi
 BINVOX = os.path.join(PROJECT_ROOT, 'src', 'data', 'binvox')
 MODELNET10_RECONS = os.path.join(PROCESSED_DIR, 'ModelNet10', 'recons')
 DOWNLOADED_STLS_DIR = os.path.join(EXTERNAL_DIR, 'downloaded_stls')
+DEMOS_DIR = os.path.join(PROJECT_ROOT, 'reports', 'demos')
